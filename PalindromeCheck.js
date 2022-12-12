6c256b27943d5a80f5d61be3caa25d63d61cb5f4
Zubaer <bijon7@yahoo.com>
2022-12-12 13:54:44 -0800

    Define is Palindrome functin with test cases

--- PalindromeCheck.js
+++ PalindromeCheck.js
@@ -3,6 +3,16 @@
  * @return {boolean}
  */
 const isPalindrome = function (x) {
+  let myFunc = x => Number(x);
   let intArr = Array.from(String(x), myFunc);
+  let tempArr = intArr.reverse();
+  let tempString = tempArr.join('');
+  let tempNumber = Number(tempString);
+  if (x === tempNumber) {
+    return true;
+  }
+  return false;
 
 };
+
+console.log(isPalindrome(-121));
