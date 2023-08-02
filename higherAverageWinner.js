12146761953d37227b41c5267f22763315c4d12b
Zubaer <bijon7@yahoo.com>
2023-08-01 23:56:07 -0700

    Add values to calculate verage scores for both teams

--- higherAverageWinner.js
+++ higherAverageWinner.js
@@ -2,8 +2,13 @@
 //competes three times in a particular event as part 
 //of a competition. A team only wins if its average 
 //score is double of the other team. Otherwise, there 
-//is no winner
+//is no winner.
 const calcAverage = (firstScore, secondScore, thirdScore) => {
   return (firstScore + secondScore + thirdScore) / 3;
 
 }
+
+// Values added for both teams for calculating average
+//score.
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
