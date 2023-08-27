fed610fb1f3f240e444aeba1574a4c90f6c30068
Zubaer <bijon7@yahoo.com>
2023-08-26 23:47:32 -0700

    Add comments defining the functions

--- higherAverageWinner.js
+++ higherAverageWinner.js
@@ -1,8 +1,10 @@
-//Takes average scores of two teams, each of which  
-//competes three times in a particular event as part 
-//of a competition. A team only wins if its average 
-//score is double of the other team. Otherwise, there 
-//is no winner.
+/*Takes average scores of two teams, each of which competes three times in a particular event as part of a competition. A team only wins if its average 
+score is double of the other team. Otherwise, there 
+is no winner
+*/
+
+
+//Defines function to calculate average score.
 const calcAverage = (firstScore, secondScore, thirdScore) => {
   return (firstScore + secondScore + thirdScore) / 3;
 
@@ -13,6 +15,9 @@ const calcAverage = (firstScore, secondScore, thirdScore) => {
 let scoreDolphins = calcAverage(44, 23, 71);
 let scoreKoalas = calcAverage(65, 54, 49);
 
+
+//Calculates winner between two values based on the fact if one value is at least twice
+//the the other value.  
 const checkWinner = function (avgDolphins, avgKoalas) {
   if (avgDolphins >= (2 * avgKoalas)) {
     console.log("Dolphins win!");
